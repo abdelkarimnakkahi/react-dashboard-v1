c0ad6e985ee7919329075dd3e79a8ca758f39ac4
Abdelkarim Nakkahi <abdelkarim.nakkahi@gmail.com>
2025-10-17 21:56:53 +0100

    add reviews chart and some responsive style

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import "./RightSide.css"
-import Updates from '../Updates/Updates';
+import React from "react";
+import "./RightSide.css";
+import Updates from "../Updates/Updates";
+import Reviews from "../Reviews/Reviews";
 
 function RightSide() {
   return (
-    <div className='right-side'>
-        <div>
-            <h3>Updates</h3>
-            <Updates />
-        </div>
-        <div>
-            <h3>Customer Reviews</h3>
-        </div>
+    <div className="right-side">
+      <div>
+        <h3>Updates</h3>
+        <Updates />
+      </div>
+      <div>
+        <h3>Customer Reviews</h3>
+        <Reviews />
+      </div>
     </div>
-  )
+  );
 }
 
 export default RightSide;
